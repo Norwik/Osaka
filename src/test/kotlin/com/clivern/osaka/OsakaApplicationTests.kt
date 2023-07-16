ed1cf7ed7f8e8781a1830dc8f50acbc70d4694aa
Clivern <hello@clivern.com>
2023-07-16 19:00:20 +0200

    init

---
+++ src/test/kotlin/com/clivern/osaka/OsakaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clivern.osaka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OsakaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
